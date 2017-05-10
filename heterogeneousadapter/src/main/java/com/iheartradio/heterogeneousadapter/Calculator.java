ceae55d8866bf6894a153f346470d8714ed7cbf1
Jonathan Muller <elforama@gmail.com>
2017-05-10 12:11:26 -0400

    testing spark

---
+++ heterogeneousadapter/src/main/java/com/iheartradio/heterogeneousadapter/Calculator.java
@@ -0,0 +1,16 @@
+package com.iheartradio.heterogeneousadapter;
+
+/**
+ * Created by Jonathan Muller on 4/28/17.
+ */
+
+public class Calculator {
+
+    public int plus(int first, int second) {
+        return first + second;
+    }
+
+    public int minus(int first, int second) {
+        return first - second;
+    }
+}
